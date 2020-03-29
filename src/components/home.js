2985759c3b865eed2ae5aa396ddf9090fad8e2dc
Ashly Lau <ashlylau@gmail.com>
2020-03-29 17:09:47 +0800

    self-host monterrat font

--- src/components/home.js
+++ src/components/home.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import {info} from './fakedata';
+require('typeface-montserrat')
 
 import image2 from "../resources/img/netflixparty.jpg";  //TODO: figure out how to use images from DB
 
