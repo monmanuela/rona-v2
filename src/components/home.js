b2c4cd1254abfdeabad0664546bf3453e0dd0135
Monika <manuela.monika97@gmail.com>
2020-03-30 13:40:15 +0800

    Get is working

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 require('typeface-montserrat');
-import {info} from './fakedata';
 import {articlesRef} from '../data/firestore'
 
 export default class Home extends React.Component {
@@ -16,38 +15,20 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-    // Make a get call
-    let articles = [];
     articlesRef.get()
       .then(snapshot => {
+        let articles = [];
         snapshot.forEach(doc => {
-          console.log("Pushing data");
           articles.push(doc.data());
         });
+        return articles
       })
-      .then(() => {
-        console.log("ARTICLES: ", articles);
-        this.setState(articles);
-        console.log("STATE ARTICLES: ", this.state.articles);
+      .then(articles => {
+        this.setState({articles});
       })
       .catch(err => {
         console.log('Error getting documents', err);
       });
-
-    // Call the firestore set listener
-    // articlesRef.onSnapshot(snapshot => {
-    //   snapshot.docChanges().forEach(change => {
-    //     console.log("CHANGE");
-    //     console.log(change.doc.data());
-    //   });
-    // }, error => {
-    //   console.log(`Encountered error: ${error}`);
-    // })
-  }
-
-  componentWillUnmount() {
-    // Detach the listener
-    // articlesRef.onSnapshot(() => {})();
   }
 
   render() {
@@ -68,45 +49,24 @@ export default class Home extends React.Component {
   }
 }
 
-class CardList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: info,
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("COMPONENT DID UPDATE WOI");
-    console.log("THIS: ", this.props.articles);
-    console.log("PREV: ", prevProps.articles);
-    if (this.props.articles !== prevProps.articles) {
-      this.setState({
-        articles: this.props.articles,
-      })
-    }
-    console.log("NOW STATE: ", this.state.articles);
-  }
-
-  render() {
-    const cardsArray = this.state.articles.map(article => {
-      return (
-        <div style={{marginBottom: '1rem'}}>
-          <Card>
-            <CardContent>
-              <Typography style={{textAlign: 'center', fontFamily: 'Montserrat', fontWeight: '600'}} variant='h3'>{article.title}</Typography>
-              <img src={article.image} alt={article.title}/>
-              <Typography style={{textAlign: 'center', fontFamily: 'Montserrat'}}>{article.description}</Typography>
-              <Typography style={{textAlign: 'end', fontFamily: 'Montserrat', marginTop: '10px', marginRight: '10px'}} color= 'textSecondary'><a href={`https://${article.link}/`} target="_blank">Link to Source</a></Typography>
-            </CardContent>
-          </Card>
-        </div>
-      )});
-
+const CardList = ({ articles }) => {
+  const cardsArray = articles.map(article => {
     return (
-      <div>
-        {cardsArray}
+      <div style={{marginBottom: '1rem'}}>
+        <Card>
+          <CardContent>
+            <Typography style={{textAlign: 'center', fontFamily: 'Montserrat', fontWeight: '600'}} variant='h3'>{article.title}</Typography>
+            <img src={article.image} alt={article.title}/>
+            <Typography style={{textAlign: 'center', fontFamily: 'Montserrat'}}>{article.description}</Typography>
+            <Typography style={{textAlign: 'end', fontFamily: 'Montserrat', marginTop: '10px', marginRight: '10px'}} color= 'textSecondary'><a href={`https://${article.link}/`} target="_blank">Link to Source</a></Typography>
+          </CardContent>
+        </Card>
       </div>
-    );
-  }
-}
+    )});
+
+  return (
+    <div>
+      {cardsArray}
+    </div>
+  );
+};
